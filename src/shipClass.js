62ce1f7df1d3c9c0a485db43c200b2404d21e033
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-08-18 15:58:24 +0500

    feat(shipClass.js): add ship module and define it's constructor

---
+++ src/shipClass.js
@@ -0,0 +1,13 @@
+// class for creating ships in the game
+export default class ships {
+  // private fields that keep track of the length of the ship the number of time it has been hit and if it has sunk or not;
+  #shipLength;
+  #numberOfHits;
+  #sinkStatus;
+  constructor(l) {
+    // initialy a ship will have zero hits and sink status is set to false because ship has not sunk yet
+    this.#shipLength = l;
+    this.#numberOfHits = 0;
+    this.#sinkStatus = false;
+  }
+}
