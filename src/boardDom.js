e37cc0e2d4196b1c63168dcc3d252c0f9340f45d
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-08-20 02:43:12 +0500

    feat(gameBoard): create ship objects for player and computer

--- src/boardDom.js
+++ src/boardDom.js
@@ -1,4 +1,6 @@
+// class for handling all dom calls for gameBoard
 class boardManipulator {
+  // takes a parent node and an array as arguement builds a grid appends it to the parent node and saves the references to the nodes in the array
   makeBoard(player, boardArray) {
     for (let i = 0; i < 100; i++) {
       boardArray[i] = document.createElement('div');
--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,7 +1,23 @@
+import shipMake from './shipClass.js';
 import { boardDom } from './boardDom.js';
 export default class gameBoard {
   #playerBoardArray = [];
   #computerBoardArray = [];
+  #playerShips = {
+    Carrier: new shipMake(5),
+    Battleship: new shipMake(4),
+    Cruiser: new shipMake(3),
+    Submarine: new shipMake(3),
+    Destroyer: new shipMake(2),
+  };
+  #enemyShips = {
+    Carrier: new shipMake(5),
+    Battleship: new shipMake(4),
+    Cruiser: new shipMake(3),
+    Submarine: new shipMake(3),
+    Destroyer: new shipMake(2),
+  };
+
   constructor(playerBoard, computerBoard) {
     boardDom.makeBoard(playerBoard, this.#playerBoardArray);
     boardDom.makeBoard(computerBoard, this.#computerBoardArray);
