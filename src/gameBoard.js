053d564fc375e3137f3bffa893894c0380f1035f
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-08-19 15:30:21 +0500

    feat(gameBoard): implement node based grid for ship placement

---
+++ src/gameBoard.js
@@ -0,0 +1,15 @@
+export default class gameBoard {
+  #boardArray = [];
+  constructor(for_player) {
+    this.#makeBoard(for_player);
+  }
+  #makeBoard(player) {
+    for (let i = 0; i < 20; i++) {
+      this.#boardArray[i] = document.createElement('div');
+      this.#boardArray[i].classList.add('gridBox');
+      this.#boardArray[i].style.height = '5px';
+      this.#boardArray[i].style.width = '5px';
+      this.#boardArray[i].setAttribute('id', `${player}GrideBox${i}`);
+    }
+  }
+}
