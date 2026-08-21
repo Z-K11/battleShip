82fa50e8ffb280b49b16701d2c36c0fdde0090ad
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-08-21 17:49:23 +0500

    test(numberisor): validate non repeating random number generation

--- src/numberisor.test.js
+++ src/numberisor.test.js
@@ -1,4 +1,5 @@
 import { numberisor } from './numberisor.js';
+import { uniqueRandomiser } from './numberisor.js';
 
 test('Returns a random number 10 and 13', () => {
   const result = numberisor(10, 13);
@@ -12,4 +13,16 @@ test('Always returns a random number between any range', () => {
     expect(result).toBeGreaterThanOrEqual(i);
     expect(result).toBeLessThan(i + 5);
   }
+});
+
+test('Returns a unique non repeating random number', () => {
+  const randomiser = uniqueRandomiser();
+  const result = randomiser();
+  let check = () => {
+    for (let i = 0; i < 99; i++) {
+      if (result === randomiser()) return false;
+    }
+    return true;
+  };
+  expect(check()).toBe(true);
 });
